b63c519872c395437f6123f7ac94d6070ce0502e
RodrigoSSoares <srn.rodrigo@gmail.com>
2015-08-18 16:31:38 -0300

    Finalizado Cadastro Usuário #GDP-9 Fixed

--- GerenciadorDePedidos.Web/Controllers/UsuariosController.cs
+++ GerenciadorDePedidos.Web/Controllers/UsuariosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GerenciadorDePedidos.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,15 @@ namespace GerenciadorDePedidos.Web.Controllers
 {
     public class UsuariosController : Controller
     {
+        public ActionResult Salvar (Usuario usuario) 
+        {
+            GerenciadorDePedidosWebContext db = new GerenciadorDePedidosWebContext();
+
+            db.Usuarios.Add(usuario);
+            db.SaveChanges();
+
+            return View("Index");
+        }
         //
         // GET: /Usuarios/
         public ActionResult Index()
