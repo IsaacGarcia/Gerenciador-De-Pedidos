26b2a1aef3232ce0199e98793d191493a17f00fe
RodrigoSSoares <srn.rodrigo@gmail.com>
2015-08-18 11:42:10 -0300

    Reestruturação do projeto

---
+++ GerenciadorDePedidos.Web/Models/Pedido.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GerenciadorDePedidos.Web.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataDoCadastro { get; set; }
+    }
+}
