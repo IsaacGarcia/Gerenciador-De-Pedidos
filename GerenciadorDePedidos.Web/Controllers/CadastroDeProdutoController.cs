cb94b0b4e46c9e0ea8765492bd3bdf27dc2849b3
SheltonMartins <sheltonmartinslucas@gmail.com>
2015-08-18 15:59:25 -0300

    Tela de Cadastro De Produtos #GDP-5

---
+++ GerenciadorDePedidos.Web/Controllers/CadastroDeProdutoController.cs
@@ -0,0 +1,33 @@
+﻿using GerenciadorDePedidos.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GerenciadorDePedidos.Web.Controllers
+{
+    public class CadastroDeProdutoController : Controller
+    {
+        //
+        // GET: /CadastroDeProduto/
+        public ActionResult Salvar(Produto produto)
+        {
+            GerenciadorDePedidosWebContext db = new GerenciadorDePedidosWebContext();
+
+            db.Produtoes.Add(produto);
+            db.SaveChanges(); ;
+
+            return Redirect("/CadastroDeProduto");
+        }
+
+        public ActionResult Index()
+        {
+            GerenciadorDePedidosWebContext db = new GerenciadorDePedidosWebContext();
+            
+            this.ViewBag.Produtos = db.Produtoes.Select(x => x);
+
+            return View();
+        }
+    }
+}
