90f7ffc2864a0398c5818f149eb1bc36e4d2e382
SheltonMartins <sheltonmartinslucas@gmail.com>
2015-08-18 14:08:48 -0300

    Criada a classe Pedidos #GDP-5

--- GerenciadorDePedidos.Web/Models/Pedido.cs
+++ GerenciadorDePedidos.Web/Models/Pedido.cs
@@ -8,7 +8,25 @@ namespace GerenciadorDePedidos.Web.Models
     public class Pedido
     {
         public int Id { get; set; }
+        public string Descricao { get; set; }
+        public List<Item> Itens { get; set; }
+        public DateTime Data { get; set; }
+        public double Valortotal { get; set; }
+
+    }
+    public class Item
+    {
+        public int Id { get; set; }
+        public Produto Produto { get; set; }
+        public int Quantidade { get; set; }
+        public double Total { get; set; }
+
+    }
+    public class Produto
+    {
+        public int Id { get; set; }
         public string Nome { get; set; }
-        public DateTime DataDoCadastro { get; set; }
+        public double Valorunitario { get; set; }
+        public string Descricao { get; set; }
     }
 }
