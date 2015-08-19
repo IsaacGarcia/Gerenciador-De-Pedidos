2453fd4d755905d36a783cf7f3af32b351dc24ac
SheltonMartins <sheltonmartinslucas@gmail.com>
2015-08-19 17:55:16 -0300

    Editar e remover produto #GDP-8

--- GerenciadorDePedidos.Web/Controllers/CadastroDeProdutoController.cs
+++ GerenciadorDePedidos.Web/Controllers/CadastroDeProdutoController.cs
@@ -1,6 +1,7 @@
 ﻿using GerenciadorDePedidos.Web.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,13 +19,36 @@ namespace GerenciadorDePedidos.Web.Controllers
             Session["Statusdousuario"] = "Logoff";
             return Redirect("/Login");
         }
-        
+
+        public ActionResult Edit(int Id)
+        {
+            GerenciadorDePedidosWebContext db = new GerenciadorDePedidosWebContext();
+            
+            Produto produto = db.Produtoes.Find(Id);
+            
+            return View(produto);
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Produto produto)
+        {
+            GerenciadorDePedidosWebContext db = new GerenciadorDePedidosWebContext();
+            if (ModelState.IsValid)
+            {
+                db.Entry(produto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(produto);
+        }
+
         public ActionResult Salvar(Produto produto)
         {
             GerenciadorDePedidosWebContext db = new GerenciadorDePedidosWebContext();
-
+            
             db.Produtoes.Add(produto);
-            db.SaveChanges(); ;
+            db.SaveChanges();
 
             return Redirect("/CadastroDeProduto");
         }
